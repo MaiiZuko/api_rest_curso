fa59beb23acdd683d42664ddb77da1f1ad213487
MaiiZuko <maiaramzucco@gmail.com>
2023-12-14 16:31:28 -0300

    classes

---
+++ src/main/java/med/voll/api/dto/medico/DadosEndereco.java
@@ -0,0 +1,21 @@
+package med.voll.api.dto.medico;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DadosEndereco {
+
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String numero;
+    private String complemento;
+    private String cidade;
+    private String uf;
+}
