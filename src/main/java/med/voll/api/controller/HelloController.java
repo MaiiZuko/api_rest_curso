e182061a04fd5c40caac9ad8d98262fbef40042d
MaiiZuko <maiaramzucco@gmail.com>
2023-12-14 14:30:53 -0300

    Medico Controller

--- src/main/java/med/voll/api/controller/HelloController.java
+++ src/main/java/med/voll/api/controller/HelloController.java
@@ -1,4 +1,4 @@
-package med.voll.api.controller;
+package med.voll.api.controller; //classe onde a gente mapeia as informações que chegarão na nossa API
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
