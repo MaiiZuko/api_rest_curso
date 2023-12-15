91343bba0cc9ea2d38653617d03afb7f0c31cbde
MaiiZuko <maiaramzucco@gmail.com>
2023-12-15 16:20:11 -0300

    Produção de dados na API

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,7 +1,10 @@
 package med.voll.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
 import med.voll.api.dto.medico.DadosCadastroMedico;
+import med.voll.api.dto.medico.DadosListagemMedico;
 import med.voll.api.dto.medico.Medico;
 import med.voll.api.dto.medico.MedicoRepository;
 
@@ -25,6 +29,12 @@ public class MedicoController {
     @Transactional //Transação ativa com o banco de dados
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){ //Para chamar o corpo da requisição - para receber cada um dos campos json separadamente - usar classe, dentro da classe declaramos o atributo com os mesmos nomes que vem do json
         repository.save(new Medico(dados));
+
+    }
+
+    @GetMapping
+    public List<DadosListagemMedico> listar() {
+        return repository.findAll().stream().map(DadosListagemMedico::new).toList();//toList para conventer para uma lista
 
     }
     
