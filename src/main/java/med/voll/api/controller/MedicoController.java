b3c3ded24c2f738aeac00e0c049123d1aa925a12
MaiiZuko <maiaramzucco@gmail.com>
2023-12-15 15:18:26 -0300

    adicionando validações

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,11 +1,13 @@
 package med.voll.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
 import med.voll.api.dto.medico.DadosCadastroMedico;
 import med.voll.api.dto.medico.Medico;
 import med.voll.api.dto.medico.MedicoRepository;
@@ -20,7 +22,8 @@ public class MedicoController {
     private MedicoRepository repository;
 
     @PostMapping
-    public void cadastrar(@RequestBody DadosCadastroMedico dados){ //Para chamar o corpo da requisição - para receber cada um dos campos json separadamente - usar classe, dentro da classe declaramos o atributo com os mesmos nomes que vem do json
+    @Transactional //Transação ativa com o banco de dados
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){ //Para chamar o corpo da requisição - para receber cada um dos campos json separadamente - usar classe, dentro da classe declaramos o atributo com os mesmos nomes que vem do json
         repository.save(new Medico(dados));
 
     }
--- src/main/java/med/voll/api/dto/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/dto/medico/DadosCadastroMedico.java
@@ -1,7 +1,26 @@
 package med.voll.api.dto.medico;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.dto.endereco.Endereco;
 
-public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, Endereco endereco) {
+public record DadosCadastroMedico(
+
+    @NotBlank //Verifica se não está vazio, nem nulo
+    String nome, 
+
+    @NotBlank @Email //Valida email
+    String email, 
+
+    @NotBlank @Pattern(regexp = "\\d{4,6}") //crm de 4 a 6 digitos
+    String crm, 
+
+    @NotNull
+    Especialidade especialidade, 
+
+    @NotNull @Valid Endereco endereco) { //valida também oq está dentro
 
 }
